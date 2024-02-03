831c1e3eeec3ccc3c137a4e933ec94f5041623f7
Chelvi Benjamin <chelvibenjamin@gmail.com>
2024-02-03 13:58:26 +0100

    mise en page html de la selection des periode et buts

---
+++ Data_Foot/src/app/championships-list/shared/interfaces/selection.interface.ts
@@ -0,0 +1,12 @@
+export class SelectionProperties {
+    isSelectedPeriode1: boolean = false;
+    isSelectedPeriode2: boolean = false;
+    isSelectedPeriode3: boolean = false;
+    isSelectedPeriode4: boolean = false;
+    isSelectedStat1: boolean = false;
+    isSelectedStat2: boolean = false;
+    isSelectedStat3: boolean = false;
+    selectedStyle: 'default-background' | 'selected-background' = 'default-background';
+    selectedTextColor: 'black' | 'white' = 'black';
+  }
+  
--- Data_Foot/src/app/statistique/statistique.component.ts
+++ Data_Foot/src/app/statistique/statistique.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GraphiqueComponent } from "../graphique/graphique.component";
 import { StatistiqueService } from '../statistique.service';
-
+import { SelectionProperties } from '../championships-list/shared/interfaces/selection.interface';
+import { GraphiqueComponent } from '../graphique/graphique.component';
 
 @Component({
-    selector: 'app-statistique',
-    standalone: true,
-    templateUrl: './statistique.component.html',
-    styleUrl: './statistique.component.css',
-    imports: [CommonModule, GraphiqueComponent]
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.css'],
+  imports:[GraphiqueComponent,CommonModule],
+  standalone:true
 })
-
-export class StatistiqueComponent implements AfterViewInit{
+export class StatistiqueComponent implements AfterViewInit {
   @ViewChild('div1') div1!: ElementRef;
   @ViewChild('div2') div2!: ElementRef;
   @ViewChild('div3') div3!: ElementRef;
@@ -24,36 +23,47 @@ export class StatistiqueComponent implements AfterViewInit{
 
   selectedPeriode: string = '';
   selectedStatistique: string = '';
+  selectedPeriodeStates: boolean[] = [false, false, false, false]; // Un élément par division période
+  selectedStatStates: boolean[] = [false, false, false]; // Un élément par division statistique
+
+
+  isSelected: SelectionProperties = new SelectionProperties();
 
   constructor(private statistiqueService: StatistiqueService) {}
 
-  //on utilise afterview car le nativeElement @viewchild est un élément undefined au momment de l'acces avec onInit
   ngAfterViewInit() {
     this.setupClickObservables();
   }
 
   setupClickObservables() {
-    // Observables pour les statistiques
-    this.statistiqueService.getClickObservable(this.div1).subscribe(() => this.selectionChanged('Période', 'Dernier march'));
-    this.statistiqueService.getClickObservable(this.div2).subscribe(() => this.selectionChanged('Période', 'Dernier mois'));
-    this.statistiqueService.getClickObservable(this.div3).subscribe(() => this.selectionChanged('Période', 'Six derniers mois'));
-    this.statistiqueService.getClickObservable(this.div4).subscribe(() => this.selectionChanged('Période', 'Saison en cours'));
 
-    // Observables pour les periodes
-    this.statistiqueService.getClickObservable(this.stat1).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués'));
-    this.statistiqueService.getClickObservable(this.stat2).subscribe(() => this.selectionChanged('Statistique', 'Ratio Victoires / Défaites'));
-    this.statistiqueService.getClickObservable(this.stat3).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués par joueur'));
+    //souscription sur les observable des periodes
+    this.statistiqueService.getClickObservable(this.div1).subscribe(() => this.selectionChanged('Période', 'Dernier match', 0, 'selectedPeriodeStates'));
+    this.statistiqueService.getClickObservable(this.div2).subscribe(() => this.selectionChanged('Période', 'Dernier mois', 1, 'selectedPeriodeStates'));
+    this.statistiqueService.getClickObservable(this.div3).subscribe(() => this.selectionChanged('Période', 'Dernier six mois', 2, 'selectedPeriodeStates'));
+    this.statistiqueService.getClickObservable(this.div4).subscribe(() => this.selectionChanged('Période', 'Derniere saison', 3, 'selectedPeriodeStates'));
+
+    //souscription sur les observable des statistiques
+    this.statistiqueService.getClickObservable(this.stat1).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués', 0, 'selectedStatStates'));
+    this.statistiqueService.getClickObservable(this.stat2).subscribe(() => this.selectionChanged('Statistique', 'Ratio Victoires / Défaites', 1, 'selectedStatStates'));
+    this.statistiqueService.getClickObservable(this.stat3).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués par joueur', 2, 'selectedStatStates'));
+
+
   }
 
-  //methode qui permet de voir la periode et la statistique actuellement selectionnées
-  selectionChanged(type: string, value: string) {
+  //fonction qui changent l'état des périodes et statistiques et récupère leurs valeurs
+  selectionChanged(type: string, value: string, index: number, statesArray: 'selectedPeriodeStates' | 'selectedStatStates') {
     if (type === 'Période') {
       this.selectedPeriode = value;
+      // Réinitialiser les états des autres #div
+      this.selectedPeriodeStates = this.selectedPeriodeStates.map((_, i) => i === index);
     } else if (type === 'Statistique') {
       this.selectedStatistique = value;
+      // Réinitialiser les états des autres #stat
+      this.selectedStatStates = this.selectedStatStates.map((_, i) => i === index);
     }
+  
+    console.log('Période sélectionnée:', this.selectedPeriode , 'Statistique sélectionnée:', this.selectedStatistique);
 
-    console.log('Période sélectionnée:', this.selectedPeriode);
-    console.log('Statistique sélectionnée:', this.selectedStatistique);
   }
 }
