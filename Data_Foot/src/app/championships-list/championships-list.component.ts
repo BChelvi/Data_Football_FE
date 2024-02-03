84d8645346db38a69e56a1bba1f02b7b6b3f0d4d
Chelvi Benjamin <chelvibenjamin@gmail.com>
2024-02-03 14:39:34 +0100

    toggle des teams

--- Data_Foot/src/app/championships-list/championships-list.component.ts
+++ Data_Foot/src/app/championships-list/championships-list.component.ts
@@ -22,20 +22,37 @@ export class ChampionshipsListComponent {
     // Injection du service ChampionshipsService via le constructeur
   constructor(private championshipsService: ChampionshipsListService) {}
 
-  //teamVisibility: { [championshipId: number]: boolean } = {};
+  teamVisibility: { [championshipId: string]: boolean } = {};
 
   ngOnInit(): void {
     this.championshipsService.loadChampionships().subscribe({
       next: (championships: ChampionshipInterface[]) => {
         this.championships = championships;
+        console.log(this.championships)
         // Initialiser l'état de visibilité pour chaque championnat
-        /*championships.forEach(championship => {
-          this.teamVisibility[championship.id] = false;
-        });*/
+        championships.forEach(championship => {
+          this.teamVisibility[championship.competition_id] = false;
+        });
       },
       // pensez a rajouter l'erreur de l'observable
     });
   }
+
+  toggleTeamVisibility(championshipId: string): void {
+    // Réinitialisez la visibilité de tous les autres championnats
+    Object.keys(this.teamVisibility).forEach(id => {
+      if (id !== championshipId) {
+        this.teamVisibility[id] = false;
+      }
+    });
+
+    // Basculez la visibilité du championnat actuel
+    this.teamVisibility[championshipId] = !this.teamVisibility[championshipId];
+  }
+    // Ajoutez une méthode pour vérifier la visibilité des équipes
+    isTeamVisible(championshipId: string): boolean {
+      return this.teamVisibility[championshipId];
+    }
 
 
 }
