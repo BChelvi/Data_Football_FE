7908a6e708cb37dec392d2787b90d7d1f8db6dc8
Chelvi Benjamin <chelvibenjamin@gmail.com>
2024-02-03 11:48:17 +0100

    maj observable periode et statistique, methode qui renvoit les periodes et statistique observées

--- Data_Foot/src/app/statistique/statistique.component.ts
+++ Data_Foot/src/app/statistique/statistique.component.ts
@@ -18,20 +18,42 @@ export class StatistiqueComponent implements AfterViewInit{
   @ViewChild('div3') div3!: ElementRef;
   @ViewChild('div4') div4!: ElementRef;
 
+  @ViewChild('stat1') stat1!: ElementRef;
+  @ViewChild('stat2') stat2!: ElementRef;
+  @ViewChild('stat3') stat3!: ElementRef;
+
+  selectedPeriode: string = '';
+  selectedStatistique: string = '';
+
   constructor(private statistiqueService: StatistiqueService) {}
 
+  //on utilise afterview car le nativeElement @viewchild est un élément undefined au momment de l'acces avec onInit
   ngAfterViewInit() {
     this.setupClickObservables();
   }
 
   setupClickObservables() {
-    this.statistiqueService.getClickObservable(this.div1).subscribe(() => this.periode_selected('Dernier march'));
-    this.statistiqueService.getClickObservable(this.div2).subscribe(() => this.periode_selected('Dernier mois'));
-    this.statistiqueService.getClickObservable(this.div3).subscribe(() => this.periode_selected('Six derniers mois'));
-    this.statistiqueService.getClickObservable(this.div4).subscribe(() => this.periode_selected('Saison en cours'));
+    // Observables pour les statistiques
+    this.statistiqueService.getClickObservable(this.div1).subscribe(() => this.selectionChanged('Période', 'Dernier march'));
+    this.statistiqueService.getClickObservable(this.div2).subscribe(() => this.selectionChanged('Période', 'Dernier mois'));
+    this.statistiqueService.getClickObservable(this.div3).subscribe(() => this.selectionChanged('Période', 'Six derniers mois'));
+    this.statistiqueService.getClickObservable(this.div4).subscribe(() => this.selectionChanged('Période', 'Saison en cours'));
+
+    // Observables pour les periodes
+    this.statistiqueService.getClickObservable(this.stat1).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués'));
+    this.statistiqueService.getClickObservable(this.stat2).subscribe(() => this.selectionChanged('Statistique', 'Ratio Victoires / Défaites'));
+    this.statistiqueService.getClickObservable(this.stat3).subscribe(() => this.selectionChanged('Statistique', 'Buts marqués par joueur'));
   }
 
-  periode_selected(nomPeriode: string) {
-    console.log('Période sélectionnée:', nomPeriode);
+  //methode qui permet de voir la periode et la statistique actuellement selectionnées
+  selectionChanged(type: string, value: string) {
+    if (type === 'Période') {
+      this.selectedPeriode = value;
+    } else if (type === 'Statistique') {
+      this.selectedStatistique = value;
+    }
+
+    console.log('Période sélectionnée:', this.selectedPeriode);
+    console.log('Statistique sélectionnée:', this.selectedStatistique);
   }
 }
